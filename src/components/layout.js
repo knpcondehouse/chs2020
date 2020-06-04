45f00a091ca13d4989d6fbbcbd86469c900476ad
knp <knp@condehouse.co.jp>
2020-06-04 20:32:02 +0900

    motion

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,8 +8,8 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
 import Header from "./header"
+import Facebookimg from "../images/facebook.png"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -29,9 +29,13 @@ const Layout = ({ children }) => {
       <div>
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <ul className="footermenu">
+            <a href="https://condehouse.co.jp" alt="conde house official site"><li>Conde House 公式サイト</li></a>
+            <a href="https://www.facebook.com/condehouse/" alt=""><li><img src="{Facebookimg}" alt="facebook logo"/></li></a>
+            <a href="https://goole.com" alt=""><li></li></a>
+            <a href="https://goole.com" alt=""><li>© {new Date().getFullYear()} Conde House</li></a>
+          </ul>
+
         </footer>
       </div>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,21 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import Image from "../images/yoneyatonerico.jpg"
 import Image2 from "../images/naotofukasawa.jpg"
 import Video1 from "../images/kotan.mp4"
 import Video2 from "../images/kyobashi.mp4"
+import Motion from "../images/motion.mp4"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div className="motiongraphic">
-      <h3>Motion Graphics</h3>
+      <video preload='auto' loop autoPlay muted>
+        <source src={Motion} type="video/mp4" />
+     Your browser does not support HTML5 video.
+     </video>
     </div>
     <section data="eventinfo" className="chs-intro cap" data-sal="slide-up" data-sal-duration="500" data-sal-delay="500"
   data-sal-easing="ease">
@@ -21,7 +24,6 @@ const IndexPage = () => (
       <p>新作・新提案を、7月オンラインにて発表。</p>
       <p>オフィスや家での過ごし方が見直され、生活スタイルや働き方が多様化する現代、カンディハウスはお客さまとともに、より豊かなくらしを考えて参ります。</p>
       <p>国内外含めた世界同時配信にてお届けいたします。ぜひご期待ください。</p>
-
     </section>
     <section data="item-videos" className="items-container">
       <h3 data-sal="slide-up" data-sal-duration="500" data-sal-delay="500" data-sal-easing="ease">New Item Debut</h3>
@@ -61,24 +63,23 @@ const IndexPage = () => (
     <section data="sales pitch" className="chs-sales" data-sal="slide-down" data-sal-duration="500" data-sal-delay="500" data-sal-easing="ease">
       <h2 className="date">Schedule<span className="innerspan">thu</span></h2>
       <h2><span className="presspreview"></span>新作プレスプレビュー</h2>
-      <div className="cap">
-        <p>7/2　KYOBASHI ・ KOTANプレビュー forプレス＆ビジネス</p>
-        <p>7/3　新作・新提案プレゼンテーションfor ビジネス</p>
+      <div className="lineup cap">
+        <p>7/2　KYOBASHI・KOTAN <br />プレビュー for プレス & ビジネス</p>
+        <p>7/3　新作・新提案プレゼンテーション for ビジネス</p>
         <p>7/4  KYOBASHI ・ KOTAN発表発売</p>
-        <p>Next Time　　主力シリーズ追加アイテム・新作シリーズ発表</p>　　
-        <p>東京支店ライブオフィスオープン・主力シリーズオフィスアイテム発表</p>
+        <div className="designers cap">
+          <div><img src={Image} alt="Tonerico" /></div>
+          <div><p>KYOBASHI <br />by <br />TONERICO:INC. HIROSHI YONEYA</p></div>
+          <div><img src={Image2} alt="Naoto Fukasawa"/></div>
+          <div><p>KOTAN <br />by <br />NAOTO FUKASAWA</p></div>
+        </div>
+        <h3><span className="comingsoonspan"></span>Next Updates</h3>
+        <p>主力シリーズ追加アイテム・<br />新作シリーズ発表</p>　　
+        <p>東京支店ライブオフィスオープン</p>
+        <p>主力シリーズオフィスアイテム発表</p>
       </div>
-      <div className="designers cap">
-        <div><img src={Image} alt="Tonerico" /></div>
-        <div><p>デザイナー名前</p></div>
-        <div><img src={Image2} alt="Naoto Fukasawa"/></div>
-        <div><p>デザイナー名前</p></div>
-      </div>
-      <h2 className="date">2020.7.2<span>thu</span>15:00 -</h2>
-      <h2>販売区分別営業プレゼンテーション</h2>
-      <p className="cap">新たな発信「CONDE HOUSE STYLE 2020 JULY」開催。7月のこの一ヶ月間は数回に渡り、木の魅力を引き出した、心地よい新作家具・新提案をオンラインにて発表いたします。オフィスや家での過ごし方が見直され、生活スタイルや働き方が多様化する現代、カンディハウスはお客さまとともに、より豊かなくらしを考えて参ります。国内外含めた世界同時配信にてお届けしますので、ぜひご期待ください。</p>
+      <Link to="/"></Link>
     </section>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
