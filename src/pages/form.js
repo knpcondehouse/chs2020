23366a974fbc46107157acb5b4db8981d8a9005d
knp <knp@condehouse.co.jp>
2020-06-11 10:23:08 +0900

    form

--- src/pages/form.js
+++ src/pages/form.js
@@ -9,19 +9,19 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Conde House Style 2020 登録フォーム" />
 
-    <form className="cap chs-form" name="CHS2020 Contact Form" method="POST" data-netlify="true" action="/thankyou">
-      <input type="hidden" name="form-name" value="Contact Form" />
+    <form name="CHS2020 Contact Form" method="POST" data-netlify="true" action="/thankyou" className="cap chs-form">
+      <input type="hidden" name="form-name" value="CHS2020 Contact Form" />
       <div>
         <label>お名前:</label>
         <input type="text" name="name" />
       </div>
       <div>
         <label>会社名:</label>
-        <input type="text" name="name" />
+        <input type="text" name="company" />
       </div>
       <div>
         <label>部署・役職:</label>
-        <input type="text" name="name" />
+        <input type="text" name="position" />
       </div>
       <div>
         <label>Eメールアドレス:</label>
@@ -37,13 +37,13 @@ const IndexPage = () => (
       </div>
       {/* event radio*/}
       <div>
-        <input type="radio" id="male" name="gender" value="male" />
+        <input type="radio" id="male" name="male" value="male" />
         <label for="male">Male</label>
 
-        <input type="radio" id="female" name="gender" value="female" />
+        <input type="radio" id="female" name="female" value="female" />
         <label for="female">Female</label>
 
-        <input type="radio" id="other" name="gender" value="other" />
+        <input type="radio" id="other" name="other" value="other" />
         <label for="other">Other</label>
       </div>
       <div>
