97c82eb3cd8fa2f429fd2d358689885f26b04138
knp <knp@condehouse.co.jp>
2020-07-30 16:08:04 +0900

    post event

--- src/pages/index.js
+++ src/pages/index.js
@@ -103,10 +103,10 @@ const IndexPage = () => (
             <img src={Sadogawa} alt="sadogawa kiyoshi profile picture" className="shadows"/>
             <h4>ゲスト: WING LUXデザイナー 佐戸川 清</h4>
             <h4>全2回<br />1回目 11:00 〜 11:40</h4>
-            <a className="brownbutton" href="http://eepurl.com/g6-tjD" alt="July 30 Registration">7/30 11:00 〜 11:40 に参加申込み</a>
+            <a className="brownbutton" alt="July 30 Registration">7/30 11:00 〜 11:40 に参加申込み</a>
             <h4 className="topmargin">2回目 14:00〜14:40</h4>
-            <a className="brownbutton" href="http://eepurl.com/g-MeIv" alt="July 30 Afternoon Registration">7/30 14：00〜14：40 に参加申込み</a>
-            <p className="topmargin left">※開始5分前よりご入室いただけます</p>
+            <a className="brownbutton" alt="July 30 Afternoon Registration">7/30 14：00〜14：40 に参加申込み</a>
+            <p className="left">→　たくさんのご参加ありがとうございました。</p>
           </div>
         </div>
       </div>
